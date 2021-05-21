61374fc9ddcb04bc92923ef61b130dd6ed031eb4
zolo <57985738@qq.com>
2021-05-21 10:48:31 +0800

    for compile

--- src/main/jni/Application.mk
+++ src/main/jni/Application.mk
@@ -1,9 +1,10 @@
-APP_STL := stlport_static
+APP_STL := c++_static
 
 APP_MODULES      := tiff png jpeg tifffactory tiffsaver tiffconverter
 
-APP_PLATFORM=android-8
+#APP_PLATFORM=android-8
 
 APP_ABI := all
+#arm64-v8a armeabi-v7a
 
 #APP_OPTIM := debug
